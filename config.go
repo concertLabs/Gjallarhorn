0c50379f738f98ab600b0f523970fa144465e55f
drmarduk <dr.marduk@gmail.com>
2017-07-17 20:11:46 +0200

    added comments and renamed Api to API

--- config.go
+++ config.go
@@ -5,12 +5,16 @@ import (
 	"io/ioutil"
 )
 
+// Config represents the json structure of the configuration file for Gjallarhorn
+// each part of the programm has its own sub-struct
 type Config struct {
 	Httpd    HttpdConfig  `json:"Httpd"`
 	Importer ImportConfig `json:"Importer"`
-	Api      ApiConfig    `json:"Api"`
+	API      APIConfig    `json:"Api"`
 }
 
+// HttpdConfig can be used as our main http frontend server with special configs
+// or we can sit behind a nginx/apache server
 type HttpdConfig struct {
 	// InternalMode decides wether to use own httpd or use
 	// other server to serve html content, eg. nginx
@@ -18,13 +22,16 @@ type HttpdConfig struct {
 	RootDir      string `json:"RootDir"`
 }
 
+// ImportConfig controls the automatic importer to scan the filesystem for new pdf files
+// and imports them in the database
 type ImportConfig struct {
 	// Folder to watch for new files
 	ScanDir     string `json:"ScanDir"`
 	UseImporter bool   `json:"UseImporter"`
 }
 
-type ApiConfig struct {
+// APIConfig I do not know what this does :) phil!!f
+type APIConfig struct {
 	Port int `json:"Port"`
 }
 

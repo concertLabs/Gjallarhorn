910f32118dda1597fb4fb8e1a3c771de428fb853
drmarduk <dr.marduk@gmail.com>
2017-07-17 20:11:09 +0200

    stuffed the global vars to the main func

--- main.go
+++ main.go
@@ -7,22 +7,17 @@ import (
 	"github.com/quiteawful/Gjallarhorn/web"
 )
 
-var (
-	WebApp   web.WebApp
-	Importer importer.Importer
-
-	configfile *string = flag.String("config", "config.json", "the config file to use")
-)
-
 func main() {
+	configfile := flag.String("config", "config.json", "the json formatted configuration file")
 	flag.Parse()
 	c, err := loadConfig(*configfile)
 	if err != nil {
 		panic(err)
 	}
-	Importer = importer.NewImporter(c.Importer.ScanDir)
-	WebApp = web.NewWebApp(c.Httpd.RootDir)
 
-	//go Importer.Run()
+	Importer := importer.NewImporter(c.Importer.ScanDir)
+	WebApp := web.NewWebApp(c.Httpd.RootDir)
+
+	go Importer.Run()
 	WebApp.Run()
 }

34759918f03dcf97cf3456588e5ac929df857f2e
drmarduk <dr.marduk@gmail.com>
2017-07-18 19:40:21 +0200

    added comments

--- importer/importer.go
+++ importer/importer.go
@@ -7,16 +7,20 @@ import (
 	"time"
 )
 
+// Importer struct
 type Importer struct {
 	InputDir  string      // pdf input directory
 	Processor chan string // process chan
 	Wait      int         // how long should the loop sleep until the next input read starts
 }
 
+// NewImporter returns a new instance of the automatic import bot
+// dir is the input directory where the importer searches for new pdf files
 func NewImporter(dir string) Importer {
 	return Importer{InputDir: dir, Processor: make(chan string), Wait: 10}
 }
 
+// Run scans for new files and sends them on a channel to the processor
 func (i *Importer) Run() {
 	for {
 		files, err := readFiles(i.InputDir)

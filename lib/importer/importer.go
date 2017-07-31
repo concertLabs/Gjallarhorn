36aca3a12fbcd10d6e9c3248f5fbdf44ff0391db
raphael <dr.marduk@gmail.com>
2017-07-31 19:00:30 +0200

    Moved the importer package to the lib folder

--- lib/importer/importer.go
+++ lib/importer/importer.go
@@ -5,23 +5,36 @@ import (
 	"log"
 	"os"
 	"time"
+
+	"github.com/quiteawful/Gjallarhorn/lib/config"
 )
 
 // Importer struct
 type Importer struct {
-	InputDir  string      // pdf input directory
-	Processor chan string // process chan
-	Wait      int         // how long should the loop sleep until the next input read starts
+	InputDir    string      // pdf input directory
+	Processor   chan string // process chan
+	Wait        int         // how long should the loop sleep until the next input read starts
+	UseImporter bool
 }
 
 // NewImporter returns a new instance of the automatic import bot
 // dir is the input directory where the importer searches for new pdf files
-func NewImporter(dir string) Importer {
-	return Importer{InputDir: dir, Processor: make(chan string), Wait: 10}
+func NewImporter(cfg config.ImportConfig) Importer {
+	return Importer{
+		InputDir:    cfg.ScanDir,
+		UseImporter: cfg.UseImporter,
+		Processor:   make(chan string),
+		Wait:        10,
+	}
 }
 
 // Run scans for new files and sends them on a channel to the processor
 func (i *Importer) Run() {
+	if !i.UseImporter {
+		log.Println("Did not start importer. UseImporter is false")
+		return
+	}
+
 	for {
 		files, err := readFiles(i.InputDir)
 		if err != nil {
